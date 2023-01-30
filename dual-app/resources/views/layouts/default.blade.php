c535508aadf9dfb2b984696cb645efd91debcb5d
Yeraybote <yeray.bote@ikasle.egibide.org>
2023-01-30 13:23:32 +0100

    Mas vistas

--- dual-app/resources/views/layouts/default.blade.php
+++ dual-app/resources/views/layouts/default.blade.php
@@ -7,7 +7,7 @@
 
     </head>
 
-    <body>
+    <body class="bg-light">
 
         <div class="container-fluid">
 
--- dual-app/resources/views/pages/darAlta.blade.php
+++ dual-app/resources/views/pages/darAlta.blade.php
@@ -1,33 +1,73 @@
 @extends('layouts.default')
 @section('content')
 <body>
-
-    <div class="container">
-        <div class="row justify-content-md-center">
-          <div class="col col-md-5">
-            <button class="btn btn-primary bg-primary bg-gradient border-0 fs-2 w-100 h-100" type="button">Empresa</button>
+    <div class="container mt-2">
+        <h2 class="text-muted"><i class="bi bi-bell-fill"></i> Dar de alta</h2>
+        <div class="row mt-3a">
+          <div class="col col-md-3">
+            <div class="card  border-3 border-primary">
+              <div class="card-body">
+                <h4 class="card-title"><i class="bi bi-justify-left"></i> GRADO</h4>
+                <p class="card-text text-muted">Crea un nuevo grado para la Universidad de Deusto.</p>
+                <a href="#" class="btn btn-primary fs-5">Crear empresa <i class="bi bi-plus-circle"></i></a>
+              </div>
+            </div>
           </div>
-          <div class="col col-md-5">
-            <button class="btn btn-primary bg-primary bg-gradient border-0 fs-2 w-100 h-100" type="button">Grado</button>
+          <div class="col col-md-3">
+            <div class="card border-3 border-primary">
+              <div class="card-body">
+                <h4 class="card-title"><i class="bi bi-person"></i> ALUMNO</h4>
+                <p class="card-text text-muted">Inscribe un nuevo alumno para la Universidad de Deusto.</p>
+                <a href="#" class="btn btn-primary fs-5">Crear alumno <i class="bi bi-plus-circle"></i></a>
+              </div>
+            </div>
           </div>
         </div>
+    </div>
 
-        <div class="row justify-content-md-center mt-3">
-            <div class="col col-md-5">
-                <button class="btn btn-primary bg-primary bg-gradient border-0 fs-2 w-100 h-100" type="button">Alumno</button>
+    <div class="container mt-4">
+        <div class="row">
+          <div class="col col-md-3">
+            <div class="card  border-3 border-primary">
+              <div class="card-body">
+                <h4 class="card-title"><i class="bi bi-building"></i> EMPRESA</h4>
+                <p class="card-text text-muted">Añade una nueva empresa para la formación dual.</p>
+                <a href="#" class="btn btn-primary fs-5">Crear empresa <i class="bi bi-plus-circle"></i></a>
+              </div>
             </div>
-            <div class="col col-md-5">
-                <button class="btn btn-primary bg-primary bg-gradient border-0 fs-2 w-100 h-100" type="button">Tutor Empresa</button>
+          </div>
+          <div class="col col-md-3">
+            <div class="card border-3 border-primary">
+              <div class="card-body">
+                <h4 class="card-title"><i class="bi bi-layers"></i> TUTOR EMPRESA</h4>
+                <p class="card-text text-muted">Inscribe un nuevo tutor de empresa para la fomación dual.</p>
+                <a href="#" class="btn btn-primary fs-5">Crear tutor empresa <i class="bi bi-plus-circle"></i></a>
+              </div>
             </div>
+          </div>
         </div>
+    </div>
 
-        <div class="row justify-content-md-center mt-3">
-            <div class="col col-md-5">
-                <button class="btn btn-primary bg-primary bg-gradient border-0 fs-2 w-100 h-100" type="button">Tutor Universidad</button>
+    <div class="container mt-4">
+        <div class="row">
+          <div class="col col-md-3">
+            <div class="card  border-3 border-primary">
+              <div class="card-body">
+                <h4 class="card-title"><i class="bi bi-journal-bookmark"></i> TUTOR UNIVERSIDAD</h4>
+                <p class="card-text text-muted">Inscribe un nuevo tutor de universidad para la Universidad de Deusto.</p>
+                <a href="#" class="btn btn-primary fs-5">Crear tutor universidad <i class="bi bi-plus-circle"></i></a>
+              </div>
             </div>
-            <div class="col col-md-5">
-                <button class="btn btn-primary bg-primary bg-gradient border-0 fs-2 w-100 h-100" type="button">Coordinador</button>
+          </div>
+          <div class="col col-md-3">
+            <div class="card border-3 border-primary">
+              <div class="card-body">
+                <h4 class="card-title"><i class="bi bi-person-circle"></i> COORDINADOR</h4>
+                <p class="card-text text-muted">Añade un nuevo coordinador para la formación dual.</p>
+                <a href="#" class="btn btn-primary fs-5">Crear coordinador <i class="bi bi-plus-circle"></i></a>
+              </div>
             </div>
+          </div>
         </div>
     </div>
 
