31eca41670147df0f7c19c4909650e40c974ac3a
Ania <aniasura.khiari@ikasle.egibide.org>
2023-02-01 17:30:05 +0100

    evalucionDiario

---
+++ dual-app/resources/views/pages/tutor/evaluacionDiario.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.default')
+@section('content')
+   
+   <div class="container">
+        <div class="text-center">
+          <h1 class="my-4">Evaluacion Diario Aprendizaje</h1>
+        </div>
+      </div>
+    
+    <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="card-title">Curso</div>
+                  </div>
+                  <div class="col-md-9">
+                    <div class="card-text">George R.R Martin</div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="card-title">Fecha</div>
+                  </div>
+                  <div class="col-md-9">
+                    <div class="card-text">22/12/22</div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="card-title">Estudiante</div>
+                  </div>
+                  <div class="col-md-9">
+                    <div class="card-text">Chasco Fernandez, Andrea</div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="card-title">Tutor universidad</div>
+                  </div>
+                  <div class="col-md-9">
+                    <div class="card-text">Juan Alberto</div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="card-title">Tutor empresa</div>
+                  </div>
+                  <div class="col-md-9">
+                    <div class="card-text">Fernandez, Ane</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+@stop
